2a56e5c224e38adc95e879a16da181d1e25c1945
bkim2021 <bkim2021@francisparker.org>
2020-02-20 21:01:04 -0800

    Data displayed from JSON

--- visualization.js
+++ visualization.js
@@ -8,42 +8,49 @@ var col2 = [];
 var col3 = [];
 var col4 = [];
 
+var entry;
+
+var TABLE_COLUMNS = 4;
+
 $(function() {
     var sheetURL = 'https://spreadsheets.google.com/feeds/cells/1EMAV9BxUnAs-Jtvn6QsIpkik5LW6odRa8bLyxc4aMlw/1/public/full?alt=json';
 
     $.getJSON(sheetURL, function(data) {
-        var entry = data.feed.entry;
+        entry = data.feed.entry;
 
-        //Updating the table
-        for(var i = 3; i < entry.length; i += 4) {
-            col1.push(entry[i].content.$t);
-            col2.push(entry[i+1].content.$t);
-            col3.push(entry[i+2].content.$t);
-            $("table").append("<tr>Yes</tr>");
-            col4.push(entry[i+3].content.$t);
+        output = "";
+
+        output += "<table>";
+
+        var newTableRow = false;
+
+        var b = 0;
+        for(i = 0; i < entry.length; i++) {
+
+            b++;
+
+            if(b == 5) {
+                b = 1;
+                newTableRow = true;
+                output += "<tr>";
+            }
+
+            output += "<th>";
+            output += entry[i].content.$t;
+            output += "</th>";
+
+            if(newTableRow == true) {
+                output += "</tr>";
+                console.log("Row");
+                newTableRow = false;
+            }
+
+            console.log("#: " + b + " Entry " + entry[i].content.$t);
         }
+        output += "</table>";
+    
+        document.getElementById("team").innerHTML = output;
 
-        $("table").append("<tr>Yes</tr>");
-
-        //Displaying the contents of the arrays
-        console.log(col1);
-        console.log(col2);
-        console.log(col3);
-        console.log(col4);
+        console.log(entry);
     })
 });
-
-function generateTable() {
-    //var numberOfTeams = teams.length;
-    //var teams = [];
-    //if(numberOfTeams > 0) {}
-
-    $("table").append("<tr>Yes</tr>");
-
-    //Table created if teams exist
-    var table = document.createElement("table");
-    table.style.width = '50%';
-    table.setAttribute('border', '1');
-    table.setAttribute('cellspacing', '0');
-    table.setAttribute('cellpadding', '5');
-}
